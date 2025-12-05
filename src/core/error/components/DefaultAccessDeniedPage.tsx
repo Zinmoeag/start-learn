f9163640b9bbd49b43eff196dd9b638a83023ac9
Zinmoeag <111127377+Zinmoeag@users.noreply.github.com>
2025-12-05 16:58:37 +0630

    add readme

--- src/core/error/components/DefaultAccessDeniedPage.tsx
+++ src/core/error/components/DefaultAccessDeniedPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import InternalServerComponent from "./DefaultInternalServerError";
 
 interface AccessDeniedPageProps {
@@ -19,8 +18,17 @@ const DefaultAccessDeniedPage = ({
   return (
     <InternalServerComponent maxWidth="480px" padded>
       <div className="flex flex-col items-center gap-4 text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.707a1 1 0 00-1.414-1.414L10 8.586 7.707 6.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z" clipRule="evenodd" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-12 w-12 text-red-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-10.707a1 1 0 00-1.414-1.414L10 8.586 7.707 6.293a1 1 0 00-1.414 1.414L8.586 10l-2.293 2.293a1 1 0 001.414 1.414L10 11.414l2.293 2.293a1 1 0 001.414-1.414L11.414 10l2.293-2.293z"
+            clipRule="evenodd"
+          />
         </svg>
 
         <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
@@ -48,6 +56,6 @@ const DefaultAccessDeniedPage = ({
       </div>
     </InternalServerComponent>
   );
-}
+};
 
 export default DefaultAccessDeniedPage;
